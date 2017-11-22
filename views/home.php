0073d1c482ca1941019c60d1212f4fda46a15ffa
Remdran <karlos287@googlemail.com>
2017-11-22 12:13:19 +0000

    Working on styling

--- views/home.php
+++ views/home.php
@@ -5,10 +5,10 @@
         <?php if ($_SESSION['id'] == null) : ?>
             <h1 class="title">Your Personal To Do List!</h1>
 
-            <h4>This is a website where you can keep track of any tasks you have to complete</h4>
+            <h4 class="titleInfo">This is a website where you can keep track of any tasks you have to complete</h4>
 
-            <p><a href="/signup">Sign up</a> if you don't already have an account</p>
-            <p><a href="/login">Login</a> here if you already have an account</p>
+            <p class="signupP"><a href="/signup">Sign up</a> if you don't already have an account</p>
+            <p class="loginP"><a href="/login">Log In</a> here if you already have an account</p>
         <?php else : ?>
             <?php header('Location:/tasks'); ?>
         <?php endif; ?>
--- views/personallist.php
+++ views/personallist.php
@@ -5,27 +5,25 @@
         <div class="taskContainer">
             <h1>Tasks</h1>
             <?php foreach ($tasks as $task) : ?>
-                <p class="tasks" contenteditable="true" data-id="<?= $task->id ?>">
+                <p class="tasks" data-id="<?= $task->id ?>">
                     <span class="listTitle"><?= $task->title; ?></span>
                     <span class="listCont"> <?= $task->body; ?></span>
-                    <button class="deleteBtn"><a href="/d?id=<?= $task->id ?>">&#10060;</a></button>
+                    <button class="editBtn" id="<?= $task->id; ?>">&#8801;</button>
+                    <button class="deleteBtn"><a id="delA" href="/d?id=<?= $task->id ?>">&#10060;</a></button>
                 </p>
 
-                <form class="form-signin edit" method="POST" action="/tasks/edit">
+                <form class="form-signin edit editForm<?= $task->id; ?>" method="POST" action="/tasks/edit">
                     <h2 class="form-signin-heading" id="formHeader">Update this entry</h2>
                     <input type="text" id="taskId" name="taskId" class="form-control" value="<?= $task->id ?>"
                            style="display:none">
-                    <input type="text" id="taskTitle" name="taskTitle" class="form-control" placeholder="Task Title"
+                    <input type="text" id="taskTitle" name="taskTitle" class="form-control" value="<?= $task->title; ?>"
                            autofocus>
                     <input type="text" name="taskBody" id="taskBody" class="form-control"
-                           placeholder="Task Description">
+                           value="<?= $task->body; ?>">
                     <button type="submit" class="btn btn-lg btn-primary btn-block" id="updateBtn">Update</button>
                 </form>
             <?php endforeach; ?>
-
         </div>
-        <span class="editInfo">To edit a task simply click on the text you wish to change</span>
-
 
         <h2 class="form-newItem-heading" id="formHeader">Add a new task</h2>
         <form class="form-newItem" method="POST" action="/tasks/new">
